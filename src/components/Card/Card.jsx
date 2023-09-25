05b9159a64fdf839fc33c541aa7b023d63951737
DoganiWho <96683644+DoganiWho@users.noreply.github.com>
2023-09-25 21:53:45 +0000

    created ItemList and Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,22 @@
 import React from "react"
 import "./card.css"
 
-export function Card(){
+export function Card({
+  title, 
+  image, 
+  body,
+  details, 
+  tags
+}){
   return (
     <div className="card">
-      <p className="card__title">title</p>
-      <div className="card__content" >content</div >
-      <div className="card__footer">footer</div >
+      <img className="card__image" src={image} alt={title} />
+      <p className="card__title">{title}</p>
+      <div className="card__body" >{body} </div >
+      <div className="card__footer">
+        <span > #{tags} </span>
+        <span> ${details} </span>
+      </div >
     </div>
   )
 }
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,11 +1,57 @@
+import {useState, useEffect} from 'react';
+
 import { Card } from "../Card/Card"
 
 import "./itemList.css"
+import "../../index.css"
+
+const URI = "https://fakestoreapi.com"
+
+const fetchProducts = async(query="products") => {
+  const data = await fetch(`${URI}/${query}`)
+    .then(res=>res.json())
+    .catch(err=>console.log(err))
+  return data
+  }
+
+const fetchCategories = async() => { const categories =
+  await fetch('https://fakestoreapi.com/products/categories')
+    .then(res=>res.json())
+    .catch(err =>console.log(err))
+  return categories                             
+}
 
 export function ItemList() {
-  return (<>
-    <Card />
-    <Card />
-  </>
+  const [products, setProducts] = useState([])
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetchProducts()
+      .then(data => {
+        setProducts(data)
+      });
+    fetchCategories()
+      .then(categories =>{
+        setCategories(categories)
+      });
+  }, []);
+  
+fetch('https://fakestoreapi.com/products/category/jewelery')
+            .then(res=>res.json()) 
+  
+  return (
+    <div className="container" >
+              { categories.map(category => (
+      <button className="category__btn" key={category}>#{category}</button>
+              ))}
+      { products.map(item => (
+          <Card key={item.id}
+            title={item.title}
+            image={item.image}
+            details={item.price}
+            body={item.description}
+            tags={item.category} />
+       ))}
+    </div>
   )
 }
