ac5d89baf929457c2c646fc6f22c5b135f811bb7
DoganiWho <96683644+DoganiWho@users.noreply.github.com>
2023-09-27 19:32:30 +0000

    improved color palette

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react"
 
-import {Inventory } from "./pages/Inventory"
-import {useTheme} from "./context/ThemeContext";
+import { Inventory } from "./pages/Inventory"
+import { useTheme } from "./context/ThemeContext";
 import "./index.css";
 
 
 export default function App() {
 
-  const {theme, setTheme} = useTheme();
-  
+  const { theme, setTheme } = useTheme();
+
   return (
-    <main className={theme? "dark" : ""}>
+    <main className={theme ? "dark" : ""}>
       <Inventory />
     </main>
   )
